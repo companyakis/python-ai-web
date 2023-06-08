f4899e2fbb6e2330605716a6e2c7da1c6af31995
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-06-08 20:51:12 +0300

    Update 01-fare_klavye_otomasyon.py

--- 01-fare_klavye_otomasyon.py
+++ 01-fare_klavye_otomasyon.py
@@ -2,4 +2,4 @@ print("Python ile fare ve klavye otomasyonu için oynatma listesi oluşturdum")
 
 print("Örneğin son videoda kendi kendine işlem yapan bir hesap makinesi nasıl yapılabilir, bunları işledik.")
 
-//https://www.youtube.com/playlist?list=PLxEcMmxTB6A0vNcKm60S4xQcEbQrJKV8o
+//                  https://www.youtube.com/playlist?list=PLxEcMmxTB6A0vNcKm60S4xQcEbQrJKV8o
